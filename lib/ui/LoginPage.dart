d705beee2e84f2882d4147e98c599020cc45aec2
dulaj dhanushanka <dulajdhanushanka@gmail.com>
2020-09-18 17:56:39 +0530

    new form builder widget added

--- lib/ui/LoginPage.dart
+++ lib/ui/LoginPage.dart
@@ -7,6 +7,7 @@ import 'package:newpostman1/ui/HomePage.dart';
 import 'package:newpostman1/ui/SignUpPage.dart';
 
 import '../globals.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key key}) : super(key: key);
@@ -16,6 +17,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormBuilderState> _formBuilderKey =
+      GlobalKey<FormBuilderState>();
   @override
   Widget build(BuildContext context) {
     final double blockHeight = Globals.blockHeight;
@@ -35,150 +38,214 @@ class _LoginPageState extends State<LoginPage> {
               width: blockWidth * 100,
               // color: Colors.green,
               alignment: Alignment.center,
-              child: Column(
-                children: [
-                  WelcomeMessageCard(
-                    title1: "welcome to new postman",
-                    title2: "Welcome back",
-                  ),
-                  SizedBox(
-                    height: blockHeight * 2,
-                  ),
-                  LoginFormField(
-                    hintText: "Enter your email",
-                    icon: Icons.email,
-                  ),
-                  SizedBox(
-                    height: blockHeight * 1,
-                  ),
-                  LoginFormField(
-                    hintText: "Enter your password",
-                    icon: Icons.lock,
-                  ),
-                  SizedBox(
-                    height: blockHeight * 5,
-                  ),
-                  Expanded(
-                      //height is blockhright*42
-                      child: Container(
-                    // color: Colors.yellow,
-                    alignment: Alignment.center,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          height: blockHeight * 5,
-                          margin: EdgeInsets.symmetric(
+              child: SingleChildScrollView(
+                child: FormBuilder(
+                  key: _formBuilderKey,
+                  child: Column(
+                    children: [
+                      WelcomeMessageCard(
+                        //height is 30
+                        title1: "welcome to new postman",
+                        title2: "Welcome back",
+                      ),
+                      SizedBox(
+                        height: blockHeight * 2,
+                      ),
+                      Card(
+                        elevation: 10,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                          15,
+                        )),
+                        margin: EdgeInsets.symmetric(
+                          horizontal: blockWidth * 5,
+                        ),
+                        child: Container(
+                          height: blockHeight * 12,
+                          // color: Colors.grey,
+                          alignment: Alignment.center,
+
+                          padding: EdgeInsets.symmetric(
                             horizontal: blockWidth * 5,
                           ),
-                          // color: Colors.green,
-                          alignment: Alignment.centerRight,
-                          child: Container(
-                            height: blockHeight * 3,
-                            // color: Colors.redAccent,
-                            alignment: Alignment.centerRight,
-                            child: AutoSizeText(
-                              "forgot password ?",
-                              style: TextStyle(
-                                fontSize: 16,
+                          child: FormBuilderTextField(
+                            attribute: "email",
+                            validators: [
+                              FormBuilderValidators.email(),
+                              FormBuilderValidators.required(),
+                            ],
+                            textAlignVertical: TextAlignVertical.center,
+                            decoration: InputDecoration(
+                              hintText: "Enter your email",
+                              prefixIcon: Icon(
+                                Icons.email,
                               ),
                             ),
                           ),
                         ),
-                        Container(
-                          margin: EdgeInsets.symmetric(
+                      ),
+                      SizedBox(
+                        height: blockHeight * 1,
+                      ),
+                      Card(
+                        elevation: 10,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                          15,
+                        )),
+                        margin: EdgeInsets.symmetric(
+                          horizontal: blockWidth * 5,
+                        ),
+                        child: Container(
+                          height: blockHeight * 12,
+                          // color: Colors.grey,
+                          alignment: Alignment.center,
+
+                          padding: EdgeInsets.symmetric(
                             horizontal: blockWidth * 5,
                           ),
-                          child: RaisedButton(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(
-                              25,
-                            )),
-                            color: Globals.mainColor,
-                            padding: EdgeInsets.all(
-                              0,
+                          child: FormBuilderTextField(
+                            attribute: "password",
+                            validators: [FormBuilderValidators.required()],
+                            textAlignVertical: TextAlignVertical.center,
+                            decoration: InputDecoration(
+                              hintText: "Enter your password",
+                              prefixIcon: Icon(
+                                Icons.lock,
+                              ),
                             ),
-                            onPressed: () {
-                              Get.to(HomePage());
-                            },
-                            child: Container(
-                              height: blockHeight * 6,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: blockHeight * 5,
+                      ),
+                      Container(
+                        height: blockHeight * 42,
+                        // color: Colors.yellow,
+                        alignment: Alignment.center,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              height: blockHeight * 5,
+                              margin: EdgeInsets.symmetric(
+                                horizontal: blockWidth * 5,
+                              ),
+                              // color: Colors.green,
+                              alignment: Alignment.centerRight,
+                              child: Container(
+                                height: blockHeight * 3,
+                                // color: Colors.redAccent,
+                                alignment: Alignment.centerRight,
+                                child: AutoSizeText(
+                                  "forgot password ?",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.symmetric(
+                                horizontal: blockWidth * 5,
+                              ),
+                              child: RaisedButton(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(
+                                  25,
+                                )),
+                                color: Globals.mainColor,
+                                padding: EdgeInsets.all(
+                                  0,
+                                ),
+                                onPressed: () {
+                                  // Get.to(HomePage());
+                                  print("validate");
+                                  _formBuilderKey.currentState
+                                      .saveAndValidate();
+                                },
+                                child: Container(
+                                  height: blockHeight * 6,
+                                  // color: Colors.green,
+                                  alignment: Alignment.center,
+                                  child: Container(
+                                    height: blockHeight * 4,
+                                    // color: Colors.redAccent,
+                                    alignment: Alignment.center,
+                                    child: AutoSizeText(
+                                      "login".toUpperCase(),
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      minFontSize: 14,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(
+                                bottom: blockHeight * 3,
+                                left: blockWidth * 5,
+                                right: blockWidth * 5,
+                              ),
+                              height: blockHeight * 3,
                               // color: Colors.green,
                               alignment: Alignment.center,
-                              child: Container(
-                                height: blockHeight * 4,
-                                // color: Colors.redAccent,
-                                alignment: Alignment.center,
-                                child: AutoSizeText(
-                                  "login".toUpperCase(),
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  minFontSize: 14,
-                                ),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                      child: Container(
+                                    // color: Colors.orange,
+                                    alignment: Alignment.center,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "Don't have an account ? ",
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                          ),
+                                        ),
+                                        GestureDetector(
+                                          onTap: () {
+                                            //create new account
+                                            Get.to(SignUpPage());
+                                          },
+                                          child: Container(
+                                              margin: EdgeInsets.only(
+                                                left: blockWidth * 2,
+                                              ),
+                                              // color: Colors.yellow,
+                                              alignment: Alignment.center,
+                                              padding: EdgeInsets.symmetric(
+                                                horizontal: blockWidth,
+                                              ),
+                                              child: Text(
+                                                "SIGNUP HERE",
+                                                style: TextStyle(
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.w500,
+                                                  color: Globals.mainColor,
+                                                ),
+                                              )),
+                                        )
+                                      ],
+                                    ),
+                                  ))
+                                ],
                               ),
                             ),
-                          ),
+                          ],
                         ),
-                        Container(
-                          margin: EdgeInsets.only(
-                            bottom: blockHeight * 3,
-                            left: blockWidth * 5,
-                            right: blockWidth * 5,
-                          ),
-                          height: blockHeight * 3,
-                          // color: Colors.green,
-                          alignment: Alignment.center,
-                          child: Row(
-                            children: [
-                              Expanded(
-                                  child: Container(
-                                // color: Colors.orange,
-                                alignment: Alignment.center,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      "Don't have an account ? ",
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                    GestureDetector(
-                                      onTap: () {
-                                        //create new account
-                                        Get.to(SignUpPage());
-                                      },
-                                      child: Container(
-                                          margin: EdgeInsets.only(
-                                            left: blockWidth * 2,
-                                          ),
-                                          // color: Colors.yellow,
-                                          alignment: Alignment.center,
-                                          padding: EdgeInsets.symmetric(
-                                            horizontal: blockWidth,
-                                          ),
-                                          child: Text(
-                                            "SIGNUP HERE",
-                                            style: TextStyle(
-                                              fontSize: 14,
-                                              fontWeight: FontWeight.w500,
-                                              color: Globals.mainColor,
-                                            ),
-                                          )),
-                                    )
-                                  ],
-                                ),
-                              ))
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ))
-                ],
+                      )
+                    ],
+                  ),
+                ),
               ),
             )
           ],
