11c5b5dbee21584a11eaf5095af77c23a45685c9
dulaj dhanushanka <dulajdhanushanka@gmail.com>
2020-11-04 02:07:45 +0530

    issues in the login page fixed

--- lib/features/authentication/presentation/LoginPageViewModel.dart
+++ lib/features/authentication/presentation/LoginPageViewModel.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 import 'package:newpostman1/features/authentication/domain/auth_service.dart';
+import 'package:newpostman1/features/loading/presentation/LoadingPage.dart';
 import 'package:newpostman1/services/snackbar_service.dart';
 import 'package:newpostman1/useful/service_locator.dart';
 import 'package:string_validator/string_validator.dart';
@@ -20,8 +22,10 @@ class LoginPageViewModel extends ChangeNotifier {
     if (isEmail(_emailtextEditingController.text.trim()) == true) {
       if (_passtextEditingController.text.trim().isNotEmpty) {
         print("user input is valid");
+        Get.to(LoadingPage(text: "Please wait"));
         authenticationService.signIn(_emailtextEditingController.text.trim(),
             _passtextEditingController.text.trim());
+        _emailtextEditingController.text = _passtextEditingController.text = "";
       } else {
         snackBarService.showSnackBar(
             "Input Error", "Please enter a valid password", true);
